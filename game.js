587a5d5f7d0a538d256a69754a3160f2179e34e9
AthenaKiriakoulis <athena.kiriakoulis@gmail.com>
2023-04-16 16:48:30 -0400

    First commits

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
 import Phaser from 'phaser'
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-new Phaser.Game()
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', './assets/sky.png');
+    this.load.image('violet-neutral', './assets/violet-neutral.png');
+}
+
+function create ()
+{
+   this.add.image(400,300,'violet-neutral');
+   this.add.image(400, 300, 'sky');
+}
+
+function update ()
+{
+}
