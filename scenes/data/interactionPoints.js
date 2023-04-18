4c6a1d5a210ff577eaa7930587a43daf065c01b5
logan630 <logandp630@outlook.com>
2023-04-18 16:11:26 -0400

    removed testing interactions

--- scenes/data/interactionPoints.js
+++ scenes/data/interactionPoints.js
@@ -9,22 +9,6 @@ import { VIOLET_X, VIOLET_Y } from "./constants.js";
 // action: () -> () - a void function w/ no parameters that modifies stats and the environment
 export const interactionPoints = [
     {
-        name: 'something', x: 420, y: 420, r: 75,
-        action: (stats) => {
-            let div = document.getElementById('gameContainer');
-            div.style.backgroundColor = '#b00b69';
-            return stats;
-        }
-    },
-    {
-        name: 'something else', x: 800, y: 420, r: 75,
-        action: (stats) => {
-            let div = document.getElementById('gameContainer');
-            div.style.backgroundColor = '#420690';
-            return stats;
-        }
-    },
-    {
         name: 'violet', x: VIOLET_X, y: VIOLET_Y, r: 50,
         action: (stats) => {
             let div = document.getElementById('gameContainer');
