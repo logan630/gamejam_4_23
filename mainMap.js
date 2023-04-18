1d696e4f07d94d22108a4308680dd17b8d09974a
adamelsawaf <adamsawaf6@gmail.com>
2023-04-18 04:34:04 -0400

    glued title scene and main map scene together

--- mainMap.js
+++ mainMap.js
@@ -1,43 +1,4 @@
-const GAME_WIDTH  = 1200;
-const GAME_HEIGHT = 900;
 let cloudVelocity = 0.75;
-
-class titleScreen extends Phaser.Scene {
-    constructor() {
-        super({key: "titleScreen"});
-    }
-
-    preload() {
-        this.load.image('stonks_titleScreen', 'assets/stonks_titleScreen.png');
-        this.load.image('skyscrapers'       , 'assets/skyscrapers.png');
-        this.load.image('clouds'            , 'assets/clouds.png');
-        this.load.image('skyColor'          , 'assets/skyColor.jpg');
-    }
-    
-    create() {
-        this.skyColor = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyColor');
-    
-        this.clouds = this.add.image(GAME_WIDTH / 2, (GAME_HEIGHT / 2) - 100, 'clouds').setScale(GAME_WIDTH / 256);
-    
-        this.skyscrapers = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyscrapers').setScale(GAME_WIDTH / 256, GAME_HEIGHT / 192);
-    
-        this.stonks = this.physics.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'stonks_titleScreen').setScale(1.5);
-        this.stonks.setVelocity(100, 200);
-        this.stonks.setBounce(1, 1);
-        this.stonks.setCollideWorldBounds(true);
-        this.stonks.setInteractive();
-
-        this.stonks.once("pointerup", function(pointer) {
-            this.scene.start("mainMap");
-        }, this);
-    }
-    
-    update() {
-        if((this.clouds.x >= GAME_WIDTH - 400) || (this.clouds.x <= 400))
-            cloudVelocity *= -1;
-        this.clouds.x += cloudVelocity;
-    }
-}
 
 class mainMap extends Phaser.Scene {
     constructor() {
@@ -142,17 +103,4 @@ class mainMap extends Phaser.Scene {
     }
 }
 
-const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    backgroundColor: '#0072bc',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    },
-    scene: [titleScreen, mainMap]
-};
-
-new Phaser.Game(config);
+module.exports = {mainMap: mainMap};
--- titleScreen.js
+++ titleScreen.js
@@ -1,48 +1,41 @@
 const GAME_WIDTH  = 1200;
 const GAME_HEIGHT = 900;
-let cloudVelocity = 0.75;
 
-let config = {
-    type: Phaser.AUTO,
-    width: GAME_WIDTH,
-    height: GAME_HEIGHT,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+class titleScreen extends Phaser.Scene {
+    constructor() {
+        super({key: "titleScreen"});
     }
-};
 
-function preload() {
-    this.load.image('stonks_titleScreen', 'assets/stonks_titleScreen.png');
-    this.load.image('skyscrapers'       , 'assets/skyscrapers.png');
-    this.load.image('clouds'            , 'assets/clouds.png');
-    this.load.image('skyColor'          , 'assets/skyColor.jpg');
+    preload() {
+        this.load.image('stonks_titleScreen', 'assets/stonks_titleScreen.png');
+        this.load.image('skyscrapers'       , 'assets/skyscrapers.png');
+        this.load.image('clouds'            , 'assets/clouds.png');
+        this.load.image('skyColor'          , 'assets/skyColor.jpg');
+    }
+    
+    create() {
+        this.skyColor = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyColor');
+    
+        this.clouds = this.add.image(GAME_WIDTH / 2, (GAME_HEIGHT / 2) - 100, 'clouds').setScale(GAME_WIDTH / 256);
+    
+        this.skyscrapers = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyscrapers').setScale(GAME_WIDTH / 256, GAME_HEIGHT / 192);
+    
+        this.stonks = this.physics.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'stonks_titleScreen').setScale(1.5);
+        this.stonks.setVelocity(100, 200);
+        this.stonks.setBounce(1, 1);
+        this.stonks.setCollideWorldBounds(true);
+        this.stonks.setInteractive();
+
+        this.stonks.once("pointerup", function(pointer) {
+            this.scene.start("mainMap");
+        }, this);
+    }
+    
+    update() {
+        if((this.clouds.x >= GAME_WIDTH - 400) || (this.clouds.x <= 400))
+            cloudVelocity *= -1;
+        this.clouds.x += cloudVelocity;
+    }
 }
 
-function create() {
-    this.skyColor = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyColor');
-
-    this.clouds = this.add.image(GAME_WIDTH / 2, (GAME_HEIGHT / 2) - 100, 'clouds').setScale(GAME_WIDTH / 256);
-
-    this.skyscrapers = this.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'skyscrapers').setScale(GAME_WIDTH / 256, GAME_HEIGHT / 192);
-
-    this.logo = this.physics.add.image(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'stonks_titleScreen').setScale(1.5);
-    this.logo.setVelocity(100, 200);
-    this.logo.setBounce(1, 1);
-    this.logo.setCollideWorldBounds(true);
-}
-
-function update() {
-    if((this.clouds.x >= GAME_WIDTH - 400) || (this.clouds.x <= 400))
-        cloudVelocity *= -1;
-    this.clouds.x += cloudVelocity;
-}
-
-new Phaser.Game(config);
+module.exports = {titleScreen: titleScreen};
